8e38cfd32b23ab1187170df712e86f0f0361fa35
unknown <madisonadams3308@gmail.com>
2025-06-19 09:24:00 -0700

    Madison added a test game to Vegas25

--- Buttons_Matrix/Buttons_Matrix.ino
+++ Buttons_Matrix/Buttons_Matrix.ino
@@ -1,26 +1,56 @@
+#include <ArduinoGraphics.h>
+#include <Arduino_HS300x.h>
 
+#include <Arduino_LPS22HB.h>
+
+#include <RTC.h>
+
+#include <Arduino_LSM6DSOX.h>
+
+
+
+#include <vl53l4cd_api.h>
+
+
+
+#include <vl53l4cd_class.h>
 
 #include <Modulino.h>
+#include <Arduino_LED_Matrix.h>
+#include <TextAnimation.h>
 
+
+#include <ArduinoGraphics.h>
+
+
+
+
+#include <Arduino_LED_Matrix.h>
+
+ModulinoPixels leds; 
+ArduinoLEDMatrix matrix; 
 
 ModulinoButtons buttons;
 
-bool button_a = true;
-bool button_b = true;
-bool button_c = true;
+bool button_0 = true;
+bool button_1 = true;
+bool button_2 = true;
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
 
-  Modulino.begin();
-
+  // Call all necessary .begin() functions
+  Modulino.begin(); 
   buttons.begin();
+  matrix.begin();
 
   buttons.setLeds(true, true, true);
 }
 
 void loop() {
-  
+  // put your main code here, to run repeatedly:
+ 
   if (buttons.update()) {
 
 
@@ -32,10 +62,9 @@ void loop() {
   // Add the text
       matrix.textFont(Font_5x7);
       matrix.beginText(0, 1, 0xFFFFFF);
-      matrix.println("0");
+      matrix.println("  0");
       matrix.endText(SCROLL_LEFT);
       matrix.endDraw();
-
 
     } else if (buttons.isPressed(1)) {
       matrix.beginDraw();
@@ -45,9 +74,10 @@ void loop() {
   // Add the text
       matrix.textFont(Font_5x7);
       matrix.beginText(0, 1, 0xFFFFFF);
-      matrix.println("1");
+      matrix.println("  1");
       matrix.endText(SCROLL_LEFT);
       matrix.endDraw();
+
 
 
     } else if (buttons.isPressed(2)) {
@@ -55,17 +85,20 @@ void loop() {
       matrix.stroke(0xFFFFFFFF);
       matrix.textScrollSpeed(50);
 
-      // Add the text
+  // Add the text
       matrix.textFont(Font_5x7);
       matrix.beginText(0, 1, 0xFFFFFF);
-      matrix.println("2");
+      matrix.println("   2");
       matrix.endText(SCROLL_LEFT);
       matrix.endDraw();
+
 
      
     }
 
 
-    buttons.setLeds(button_0, button_1, button_2);
+  buttons.setLeds(button_0, button_1, button_2);
   }
+
+
 }
